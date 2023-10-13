67d74bc05a68522ab2244d3778a5951cf2deedd3
Manoj <manojnathsaud@gmail.com>
2023-10-13 12:44:25 +0530

    add this controller

---
+++ controllers/delegateController.js
@@ -0,0 +1,26 @@
+const delegate = require("../model/delegate");
+
+
+// Create Delegate
+const delegate_create = async (req, res) => {};
+
+// All Delegate
+const delegate_all = async (req, res) => {};
+
+// Single Delegate
+const delegate_details = async (req, res) => {};
+
+// Update Delegate
+const delegate_update = async (req, res) => {};
+
+// Delete Delegate
+const delegate_delete = async (req, res) => {};
+
+
+module.exports = {
+    delegate_create,
+    delegate_all,
+    delegate_details,
+    delegate_update,
+    delegate_delete
+}
