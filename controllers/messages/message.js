37a24cf1420d3cd78af2b9c0dc56eb3e212c9400
tns <amrutanshup@gmail.com>
2024-01-05 21:42:24 +0530

    duplicate email

--- controllers/messages/message.js
+++ controllers/messages/message.js
@@ -1,6 +1,7 @@
 module.exports = {
 SCAN_SUCCESSFULLY : "Prasad Verified successfully",
 NO_DEVOTEEFOUND : "No devotee found with this code !",
+EXISTING_DEVOTEE : "Devotee already exist !",
 BLACKLISTED_DEVOTEE_SCAN : "Blacklisted Devotee !",
 PRASAD_TAKEN: "Prasad already taken !",
 INVALID_TIME: "Prasad can not served on this time !" 
