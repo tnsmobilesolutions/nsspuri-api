d09ef986cb100cbaa1fb02c705df4716982bb10c
tns <amrutanshup@gmail.com>
2024-04-02 23:28:01 +0530

    devoteeDetails

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -35,7 +35,25 @@ async function getSingleEvent(req,res) {
 
 async function getAllEvent(req,res) {
     try {
-        createdData =  await allmodel.eventModel.find({eventId:req.params.eventId,eventAttendance:true}).sort({createdAt:-1});
+        // createdData =  await allmodel.eventModel.find({eventId:req.params.eventId,eventAttendance:true}).sort({createdAt:-1});
+        createdData =  await allmodel.eventModel.aggregate([{
+            $match: {
+              $and:[
+                {eventId:req.params.eventId},
+                {eventAttendance:true}
+              ]
+            },},
+            {  $lookup: {
+              from: 'devotees',
+              localField: 'devoteeCode',
+              foreignField: 'devoteeCode',
+              as: 'devotee'
+            }
+          },
+           {
+             $unwind: '$devotee'
+           }
+          ])
         res.json(createdData)
          } catch (error) {
              console.log('createEventError',error)
