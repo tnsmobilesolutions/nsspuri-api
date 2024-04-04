f79b7ce4b8d4d526fc7d025ddb916bc4d4db0dcd
tns <amrutanshup@gmail.com>
2024-04-04 22:57:18 +0530

    pagination

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -35,8 +35,12 @@ async function getSingleEvent(req,res) {
 
 async function getAllEvent(req,res) {
     try {
-        // createdData =  await allmodel.eventModel.find({eventId:req.params.eventId,eventAttendance:true}).sort({createdAt:-1});
-        createdData =  await allmodel.eventModel.aggregate([{
+        const currentPage = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5000;
+        const numberofskipdata = (currentPage - 1) * limit;
+        let count
+        let pipeline = []
+        pipeline.push({
             $match: {
               $and:[
                 {eventId:req.params.eventId},
@@ -53,11 +57,60 @@ async function getAllEvent(req,res) {
            {
              $unwind: '$devotee'
            },
-           {
-             $sort: {'createdAt':-1}
-           }
-          ])
-        res.json(createdData)
+           )
+        if(req.query.search && req.query.searchBy){
+            if(req.query.searchBy == "name"){
+pipeline.push({
+    $match: {
+        'devotee.name': {"$regex": `${req.query.search}`, '$options': 'i' }
+    },},)
+        }else if(req.query.searchBy == "sangha"){
+            pipeline.push({
+                $match: {
+                    'devotee.sangha': {"$regex": `${req.query.search}`, '$options': 'i' }
+                },},)
+   
+        }else if(req.query.searchBy == "bloodGroup"){
+            pipeline.push({
+                $match: {
+                    'devotee.bloodGroup': {"$regex": `${req.query.search}`, '$options': 'i' }
+                },},)
+   
+        }else if(req.query.searchBy == "mobileNumber"){
+
+            pipeline.push({
+                $match: {
+                    'devotee.mobileNumber': {"$regex": `${req.query.search}`, '$options': 'i' }
+                },},)
+        }else if(req.query.searchBy == "emailId"){
+
+            pipeline.push({
+                $match: {
+                    'devotee.emailId': {"$regex": `${req.query.search}`, '$options': 'i' }
+                },},)
+        }else if(req.query.searchBy == "status"){
+            pipeline.push({
+                $match: {
+                    'devotee.status': {"$regex": `${req.query.search}`, '$options': 'i' }
+                },},)
+        }
+       
+        }
+            pipeline.push({
+                $sort: {'createdAt':-1}
+              })
+           
+              createdData =  await allmodel.eventModel.aggregate(pipeline)
+              count =createdData.length
+              const totalPages = Math.ceil(count / limit);
+              pipeline.push({
+                $skip: numberofskipdata
+              }, {
+                $limit: limit
+              });
+              data =  await allmodel.eventModel.aggregate(pipeline)
+
+              res.json({data,count,totalPages,currentPage})
          } catch (error) {
              console.log('createEventError',error)
              res.json('createEventError',error)
